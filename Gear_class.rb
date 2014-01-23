7dfee6b13cde44a65bfdd5840cba66b2663d9681
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-23 20:58:00 +0000

    Implemented gear_inches

---
+++ Gear_class.rb
@@ -0,0 +1,27 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tyre
+   def initialize(chainring, cog, rim, tyre)
+    @chainring = chainring
+    @cog       = cog
+    @rim       = rim
+    @tyre      = tyre
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # tyre does around the rim twice for diameter
+    ratio * (rim + (tyre * 2))
+  end
+end
+
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches                      # 137.0909090909091
+puts Gear.new(52, 11, 24, 1.25).gear_inches                     # 125.27272727272728
+
+
+
+
+
---
+++ spec/Gear_class_spec.rb
@@ -0,0 +1,14 @@
+require './Gear_class'
+
+describe "Gear" do
+  
+it "chainring should equal cog" do
+  expect(initialise(chainring, cog)).to eq(chainring, cog)
+end
+
+
+
+
+
+
+end
